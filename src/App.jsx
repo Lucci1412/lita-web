1f9c59b8836b7ead490651e04d5fdb0df5739d4c
Lucci1412 <cubebaothu@gmail.com>
2023-06-06 17:24:22 +0700

    setup material

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import Button from '@mui/material/Button'
+import IconButton from '@mui/material/IconButton'
+import AlarmIcon from '@mui/icons-material/Alarm'
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart'
 function App() {
-  const [count, setCount] = useState(0)
-  // eslint-disable-next-line no-constant-condition
-  if (1 == 1) {
-    return true
-    // eslint-disable-next-line no-constant-condition
-  } else if (1 == 2) {
-    return false
-  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+      <br />
+      <IconButton color="secondary" aria-label="add an alarm">
+        <AlarmIcon />
+      </IconButton>
+      <IconButton color="primary" aria-label="add to shopping cart">
+        <AddShoppingCartIcon />
+      </IconButton>
     </>
   )
 }
