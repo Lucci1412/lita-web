4e1ca32ea3ae834a81051ccf9bfd94937a4e343f
Lucci1412 <cubebaothu@gmail.com>
2023-06-06 20:44:06 +0700

    setup dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,23 @@ import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
 import AlarmIcon from '@mui/icons-material/Alarm'
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart'
+import { useColorScheme } from '@mui/material/styles'
 function App() {
-
+  function ModeToggle() {
+    const { mode, setMode } = useColorScheme()
+    return (
+      <Button
+        onClick={() => {
+          setMode(mode === 'light' ? 'dark' : 'light')
+        }}
+      >
+        {mode === 'light' ? 'Turn dark' : 'Turn light'}
+      </Button>
+    )
+  }
   return (
     <>
+      <ModeToggle />
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
